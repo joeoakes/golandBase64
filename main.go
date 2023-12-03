c65b698c5e1bfec3b2b1dc5c95afb8ab58ea79c3
joeoakes <jxo19@psu.edu>
2023-12-03 12:56:24 -0500

    commit

--- main.go
+++ main.go
@@ -10,15 +10,18 @@ func main() {
 	data := "abc123!?$*&()'-=@~"
 
 	//Encode the String
+	fmt.Println("Encoding the String: " + data)
 	sEnc := b64.StdEncoding.EncodeToString([]byte(data))
 	fmt.Println(sEnc)
 
 	//Decode the String
+	fmt.Println("\nDecoding the String: " + sEnc)
 	sDec, _ := b64.StdEncoding.DecodeString(sEnc)
 	fmt.Println(string(sDec))
 	fmt.Println()
 
 	//This encodes/decodes using a URL-compatible base64 format.
+	fmt.Println("URL encoding/decoding")
 	uEnc := b64.URLEncoding.EncodeToString([]byte(data))
 	fmt.Println(uEnc)
 	uDec, _ := b64.URLEncoding.DecodeString(uEnc)
